1145b0812273e0bc6d3fda4a78dea57a90ba5858
nutrir <nutrir@srv-nutrir-beta.rectorado.untref.edu.ar>
2024-10-28 20:46:08 -0300

    fix merge

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 const withPWA = require('next-pwa')({
-<<<<<<< HEAD
-  dest: 'public'
-  // disable: process.env.NODE_ENV === 'development',
-  // register: true,
-  // scope: '/app',
-  // sw: 'service-worker.js',
-  //...
-})
-
-module.exports = withPWA({
-  // next.js config
-  images: {
-    domains: ['50.116.44.91'],
-  },
-})
-=======
     dest: 'public',
     // disable: process.env.NODE_ENV === 'development',
     // register: true,
@@ -31,4 +15,3 @@ module.exports = withPWA({
         domains: ['50.116.44.91', '127.0.0.1'],
     },
 });
->>>>>>> ac886fe22ec33c415b52d84bbc231755fa91e88c
