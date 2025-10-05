c4b63d106e9ffa81f378ecf1b1d67398397d576e
Federico Moix <federicomoix@Mac-mini-de-Federico.local>
2025-10-04 21:19:02 -0300

    parche

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const withPWA = require('next-pwa')({
     dest: 'public',
-    // disable: process.env.NODE_ENV === 'development',
+    disable: true, // Deshabilitar PWA completamente para debug
     // register: true,
     // scope: '/app',
     // sw: 'service-worker.js',
